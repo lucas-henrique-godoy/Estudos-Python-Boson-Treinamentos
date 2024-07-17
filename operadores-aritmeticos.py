df2a7da71aa164caf9d6c5343d0995a9efa6f4d5
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-17 15:26:58 -0300

    criei o exemplo de operadores aritméticos com python,  e como solicitar que o usuário digite algo.

--- operadores-aritmeticos.py
+++ operadores-aritmeticos.py
@@ -1,3 +1,25 @@
 x = y = z = 0
-x = 7
-y = 9
+# Aqui, três variáveis (x, y, z) estão sendo inicializadas com o valor 0.
+# A expressão x = y = z = 0 atribui o valor 0 para todas as três variáveis simultaneamente.
+
+# x = 7 Atribuindo valores as variáveis
+# y = 9
+
+x = int(input('Digite um número: '))
+# Aqui, o programa solicita que o usuário digite um número.
+# A função input() captura a entrada do usuário como uma string.
+# A função int() converte essa string em um número inteiro e o atribui à variável x.
+
+y = int(input('Digite outro número: '))
+# Aqui, o programa solicita que o usuário digite outro número.
+# Novamente, a função input() captura a entrada do usuário como uma string.
+# A função int() converte essa string em um número inteiro e o atribui à variável y.
+
+z = x + y
+# Aqui, a variável z está recebendo a soma dos valores armazenados em x e y.
+# A operação x + y é realizada e o resultado é atribuído à variável z.
+
+print('A soma dos dois valores é: ', z)
+# Esta linha exibe uma mensagem na tela junto com o valor armazenado em z.
+# A mensagem 'A soma dos dois valores é: ' é exibida seguida pelo valor de z.
+# Isso informa ao usuário a soma dos dois números que foram digitados.
