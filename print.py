731dd5ca2593b9216abb7d3687d291d3bf46bd99
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-18 12:45:46 -0300

    Update print.py

---
+++ print.py
@@ -0,0 +1,32 @@
+# FUNÇÃO PRINT()
+# Sintaxe: 
+# print(objetos, argumentos)
+#_______________________________________________________________________________________________________________
+
+# mensagem = 'Função print()'
+# print(mensagem)
+# print('Aula de Python')
+#_______________________________________________________________________________________________________________
+
+# nome = 'Lucas Henrique Godoy'
+# canal = 'Bóson Treinamentos'
+# print(canal,'-', nome)
+#_______________________________________________________________________________________________________________
+
+# nome = input('Digite seu nome: ')
+
+# msg = 'Olá ' + nome + '! Bem-vindo ao curso de Python!'
+# print(msg)
+#            # OU #
+# print('Olá ' + nome + '! Bem-vindo ao curso de Python!')
+
+# print('Outro texto')
+
+#_______________________________________________________________________________________________________________
+
+
+print('Imprime a mensagem e muda de linha')
+print('Imprime a mensagem e permanece na linha.', end='')
+print(' Continuo na mesma linha!')
+
+
