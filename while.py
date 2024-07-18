3b041eed2649cba8306f5a9738e496e61e5a4b40
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-18 16:50:25 -0300

    Criei while.py

---
+++ while.py
@@ -0,0 +1,19 @@
+# ESTRUTURA DE REPETIÇÃO WHILE
+
+# num = 1
+
+# while (num <= 10):
+#     print(num)
+#     num += 1
+# print('Laço encerrado!')
+
+#_______________________________________________________________________________________________________________________________________________________________
+
+nome = None
+
+while True:
+    print('Digite seu nome, ou x para parar:')
+    nome = input()
+    if nome == 'x' or nome == 'X':
+        break
+    print(f'Bem-vindo, {nome}')
