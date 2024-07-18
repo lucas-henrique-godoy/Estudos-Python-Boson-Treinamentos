af3c8560c83d0d8a653d6a3ed27ac6303f9490a5
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-18 15:57:36 -0300

    Update print.py

--- print.py
+++ print.py
@@ -4,21 +4,21 @@
 # print(objetos, argumentos)
 # A função print() é usada para exibir mensagens ou valores no console. Ela pode receber vários argumentos separados por vírgula.
 
-#_______________________________________________________________________________________________________________
+#____________________________________________________________________________________________________________________________________
 
 # Exemplo básico de uso da função print():
 mensagem = 'Função print()'
 print(mensagem)  # Imprime 'Função print()' no console
 print('Aula de Python')  # Imprime 'Aula de Python' no console
 
-#_______________________________________________________________________________________________________________
+#___________________________________________________________________________________________________________________________________
 
 # Concatenação de strings usando print():
 nome = 'Lucas Henrique Godoy'
 canal = 'Bóson Treinamentos'
 print(canal, '-', nome)  # Imprime 'Bóson Treinamentos - Lucas Henrique Godoy' no console
 
-#_______________________________________________________________________________________________________________
+#____________________________________________________________________________________________________________________________________
 
 # Usando input() para capturar entrada do usuário e formatando a saída:
 nome = input('Digite seu nome: ')  # Solicita ao usuário que digite seu nome
@@ -30,14 +30,14 @@ print('Olá ' + nome + '! Bem-vindo ao curso de Python!')  # Versão simplificad
 
 print('Outro texto')  # Imprime 'Outro texto' em uma nova linha
 
-#_______________________________________________________________________________________________________________
+#____________________________________________________________________________________________________________________________________
 
 # Modificando o comportamento de quebra de linha usando o argumento end:
 print('Imprime a mensagem e muda de linha')
 print('Imprime a mensagem e permanece na linha.', end='')  # Não muda de linha após essa mensagem
 print(' Continuo na mesma linha!')  # Continua na mesma linha da mensagem anterior
 
-#_______________________________________________________________________________________________________________
+#____________________________________________________________________________________________________________________________________
 
 # Utilizando formatação de string com o método format():
 nome = 'Maria'
@@ -49,7 +49,7 @@ print('O nome dela é {0} e ela tem {1} anos'.format(nome, idade))
 msg_formatada = 'O nome dela é {0} e ela tem {1} anos'.format(nome, idade)
 print(msg_formatada)  # Imprime a mensagem formatada armazenada na variável
 
-#_______________________________________________________________________________________________________________
+#______________________________________________________________________________________________________________________________________
 
 # F STRING: forma moderna de formatação de strings introduzida no Python 3.6
 nome = 'Lucas'
@@ -61,7 +61,7 @@ print(f'Olá, meu nome é {nome} e meu peso é {peso} quilos')
 msg = f'Olá, meu nome é {nome} e meu peso é {peso} quilos.'
 print(msg)  # Imprime a mensagem formatada armazenada na variável
 
-#_______________________________________________________________________________________________________________
+#_______________________________________________________________________________________________________________________________________________________
 
 # F STRING EXECUTANDO OPERAÇÕES: é possível realizar operações dentro de uma f-string
 a = 10
@@ -69,24 +69,24 @@ b = 5
 res = f'A soma de {a} com {b} é igual a: {a + b}'  
 print(res)  # Imprime a mensagem com o resultado da operação dentro da f-string: 'A soma de 10 com 5 é igual a: 15'
 
-#_______________________________________________________________________________________________________________
+#________________________________________________________________________________________________________________________________________________________
 
 # F STRING EXECUTANDO OPERAÇÕES. EXEMPLO 2: formatação de números com f-string
 valor = 125.579637
 print(f'O valor é: {valor:.2f}')  
-# Imprime o valor arredondado com duas casas decimais: 'O valor é: 125.58'
+# Imprime o valor arredondado com duas casas decimais: 'O valor é: 125.58'. Usando :.2f
 
-#_______________________________________________________________________________________________________________
+#_________________________________________________________________________________________________________________________________________________________
 
 # CARACTERE DE ESCAPE: uso de caracteres de escape em f-strings
 valor = 125.579637
 print(f'O valor é: \'{valor:.2f}\'')  
-# Imprime o valor entre aspas simples usando o caractere de escape \': 'O valor é: '125.58''
+# Imprime o valor entre aspas simples usando o caractere de escape \': 'O valor é: '125.58''. Aqui a intenção foi mostrar também as aspas junto com o valor..
 # Caso seja necessário mostrar numa situação o caractere de \ é só usar \\ para mostrar 1 barra.
 
-#_______________________________________________________________________________________________________________
+#____________________________________________________________________________________________________________________________________________________________
 
-# CARACTERE DE ESCAPE - TABULAÇÃO \t: uso do caractere de escape \t para tabulação
+# CARACTERE DE ESCAPE - TABULAÇÃO(TAB DO TECLADO) \t: uso do caractere de escape \t para tabulação. Dando assim espaço entre os elementos.
 nome = 'Lucas'
 idade = 26
 print(f'Nome: {nome}\tIdade: {idade}')  
