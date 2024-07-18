632d8f3d667920a12cf7358a7c1227e6af3966bf
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-18 13:19:08 -0300

    Update print.py

--- print.py
+++ print.py
@@ -24,9 +24,15 @@
 
 #_______________________________________________________________________________________________________________
 
+# print('Imprime a mensagem e muda de linha')
+# print('Imprime a mensagem e permanece na linha.', end='')
+# print(' Continuo na mesma linha!')
 
-print('Imprime a mensagem e muda de linha')
-print('Imprime a mensagem e permanece na linha.', end='')
-print(' Continuo na mesma linha!')
+#_______________________________________________________________________________________________________________
+
+nome = 'Maria'
+idade = 30
+msg_formatada = 'O nome dela é {0} e ela tem {1} anos'.format(nome,idade)
+
 
 
