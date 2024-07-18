319cd5870ac6a1deb114f1b848cbd5e787f9e26f
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-18 13:30:14 -0300

    Update print.py

--- print.py
+++ print.py
@@ -32,7 +32,8 @@
 
 nome = 'Maria'
 idade = 30
-msg_formatada = 'O nome dela é {0} e ela tem {1} anos'.format(nome,idade)
+msg_formatada = 'O nome dela é {0} e ela tem {1} anos'.format(nome, idade)
+print(msg_formatada)
 
 
 
