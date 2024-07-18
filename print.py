87e51e634ab52bb203f422a665219ea6c9ea38d7
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-18 13:36:25 -0300

    mudei a ordem dos códigos.

--- print.py
+++ print.py
@@ -32,8 +32,9 @@
 
 nome = 'Maria'
 idade = 30
+print('O nome dela é {0} e ela tem {1} anos'.format(nome, idade))
+
+        # OU #
 msg_formatada = 'O nome dela é {0} e ela tem {1} anos'.format(nome, idade)
 print(msg_formatada)
-
-
-
+        
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+msg_formatada = 'O nome dela é {0} e ela tem {1} anos'.format(nome, idade)
+# print(msg_formatada)
