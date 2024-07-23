0aea5e2033280d167309592701fadd24277cd743
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-23 16:55:55 -0300

    Update listas.py

--- listas.py
+++ listas.py
@@ -10,6 +10,8 @@ n2 = [1,6,3,0,12,11]
 valores = n1 + n2
 # print(valores)
 # print(type(valores))
+# print(valores[-1]) -> acessa o ultimo valor da lista
+# print(valores[-2]) -> acessa o penultimo valor da lista
 valores[0] = 9
 # print(valores[0:2])
 # print(valores[:4])
