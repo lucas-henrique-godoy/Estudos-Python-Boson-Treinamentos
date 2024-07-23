91987b1bddcfacabbce5cafe920a0d4652fa80f6
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-23 12:35:39 -0300

    Create listas.py

---
+++ listas.py
@@ -0,0 +1,16 @@
+# Lista: representa um sequência de valores
+
+# Sintaxe: nome_lista = [valores]
+
+# notas = [5,7,8,6,9]
+# print(notas)
+
+n1 = [4,6,7,8,0,3]
+n2 = [1,6,3,0,12,11]
+valores = n1 + n2
+# print(valores)
+# print(type(valores))
+valores[0] = 9
+# print(valores[0:2])
+# print(valores[:4])
+print(valores[3:4])
