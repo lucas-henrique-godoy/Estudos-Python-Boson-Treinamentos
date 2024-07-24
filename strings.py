bdf82452b8e147c8976b3890685bdc2c46d0aa6b
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-24 16:36:20 -0300

    criei o strings.py

---
+++ strings.py
@@ -0,0 +1,14 @@
+# STRINGS
+
+# EXEMPLO 1
+# nome = 'Curso de Python'
+# instrutor = ' Fábio'
+# letra = nome[2]
+# print(nome + ' com ' + instrutor)
+# print(len(nome))
+
+#________________________________________________________________________________________________________________
+#EXEMPLO 2
+frase = 'Vamos aprender python hoje.'
+palavras = frase.split() # separa cada pavra e transforma em um item de uma lista que podem ser acessadas invidualmente incluise utilizando []
+print(palavras)
