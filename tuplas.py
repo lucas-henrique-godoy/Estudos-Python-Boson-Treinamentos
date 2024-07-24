b43f79d5a08a7591cd0a43b0d2a51ef86885391f
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-24 13:21:27 -0300

    criei o tuplas.py

---
+++ tuplas.py
@@ -0,0 +1,15 @@
+# TUPLAS: SÃO IMUTÁVEIS. NÃO É POSSÍVEL ALTERAR CONTEÚDO DA TUPLA DEPOIS DA TUPLA TER SIDO CRIADA. PODEM CONTER QUAISQUER TIPOS DE DADOS
+
+# EXEMPLO DE TUPLA:
+#tupla = (2,4,6,7,9)
+# tupla[0] = 1 *Não é possivel modificar ou atribuir novos valores a uma tupla depois de ela ja ter sido criada.
+#print(tupla)
+#print(tupla[-1]) -> Acessa o ultimo valor da tupla
+#____________________________________________________________________________________________________________
+
+halogenicos = ('F', 'Cl', 'Br', 'I', 'At')
+gases_nobres = ('He', 'Ne', 'Ar', 'Xe', 'Kr', 'Rn')
+elementos = halogenicos + gases_nobres
+#t1 = (5,2,6,8,4,5,6,4,4,0,12,22,3,4,5)
+print(elementos[0:6])
+
