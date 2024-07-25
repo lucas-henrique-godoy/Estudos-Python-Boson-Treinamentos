e13653446abbb8a9197759a5777af5146463192c
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-25 08:29:05 -0300

    adicionei anotações sobre dicionarios em python

--- dicionarios.py
+++ dicionarios.py
@@ -1,1 +1,7 @@
-# Dicionários: Permitem armazenar dados em pares chave valor. São similares à arrays associativos que existem em outras linguagens ou hach maps.
+# Dicionários: Permitem armazenar dados em pares chave valor. São similares à arrays associativos que existem em outras linguagens ou hach maps. Não permitem itens duplicados, masa os itens podem ser de qualquer tipo.
+#  A chave é uma string que da acesso a um valor armazenado.
+
+
+elemento = {
+
+}
