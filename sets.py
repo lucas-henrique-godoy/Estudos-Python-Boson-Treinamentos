860585bd82555c486a4b60aa989eaafaf344b198
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-26 14:00:44 -0300

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,3 @@
+# Set: Conjuntos/coleções não ordenadas de valores únicos usados para armazenar multiplos itens dentro de um objeto.
+# - Apenas armazenam itens não duplicados e suportam operações matemáticas sobre conjuntos: União, Intersecção, etc.
+# - Não é possível modificar os itens dentro de um set, mas conseguimos adicionar novos elementos. Portanto o set em si, o conjunto é mutável.
