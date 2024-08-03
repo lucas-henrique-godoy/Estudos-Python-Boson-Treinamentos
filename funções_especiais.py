813bcf97fd982be4d5af4c26d7c8973f134a74bc
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-08-03 00:15:07 -0300

    Update funções_especiais.py

--- funções_especiais.py
+++ funções_especiais.py
@@ -5,7 +5,7 @@
 # lambda argumentos: expressão
 
 # EX1 - FUNÇÃO LAMBDA QUE RETORNE O QUADRADO DE UM NÚMERO
-# quadrado = lambda x: x**2 
+# quadrado = lambda x: x**2
 
 # for i in range(1,11):
 #     print(quadrado(i))
@@ -25,19 +25,19 @@
 
 #               FUNÇÃO MAP()
 
-# Sintaxe: # map(função, iterável) 
+# Sintaxe: # map(função, iterável)
 # Cada função que for passada sera aplicada para cada item dentro desse objeto iterável. Ex: Lista, tupla, dicionário. Pode ser uma função normal def ou lambda.
 # OBS: O iterável é o obejto que vai receber essa função.
 
 #EXEMPLO 1 : FUNÇÃO DE DOBRAR OS ITENS DE UMA LISTA USANDO MAP E LAMBDA.
-num = [1,2,3,4,5,6,7,8]
-dobro = list(map(lambda x: x * 2, num))
-print(dobro)
+# num = [1,2,3,4,5,6,7,8]
+# dobro = list(map(lambda x: x * 2, num))
+# print(dobro)
 
-#EXEMPLO 2 : FUNÇÃO PARA CONVERTER AS STRINGS PARA MAIÚSCULAS.
-palavras = ['Python', 'é', 'uma', 'linguagem', 'de', 'programação']
-maiúsculas = list(map(str.upper, palavras))
-print(maiúsculas)
+# #EXEMPLO 2 : FUNÇÃO PARA CONVERTER AS STRINGS PARA MAIÚSCULAS.
+# palavras = ['Python', 'é', 'uma', 'linguagem', 'de', 'programação']
+# maiúsculas = list(map(str.upper, palavras))
+# print(maiúsculas)
 #_____________________________________________________________________________________________________________________
 
 # A idéia da função lambda e map, são idéas que vem da programação funcional, que é um paradigma distinto da procedural, orientada a objetos. python tem suporte à programação funcional, por isso possui essas funções disponíveis.
@@ -46,17 +46,44 @@ print(maiúsculas)
 #_____________________________________________________________________________________________________________________
 
 # FUNÇÃO FILTER():
-# Filtra elementos de uma sequencia. Itera sobre uma sequencia e vai extrair determinados elementos de dentro dessa sequencia de acordo com os critérios que a gente estabelece. Geralmente usada em conjunto ou como complemento da função map.
+# Filtra elementos de uma sequencia. Itera sobre uma sequencia e vai extrair determinados elementos de dentro dessa sequencia de acordo com os critérios que a gente estabelece. Geralmente usada em conjunto ou como complemento da função map.  A idéia geral é: a função é aplicada sobre uma lista de valores e para cada vez que  a função ser aplicada retornar verdadeiro, o valor em si que é retornado e não true ou false.
 # SINTAXE: filter(função, sequencia)
 
-def numeros_pares(n):
-    return n % 2 == 0
 
-numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+# FUNÇÃO QUE VERIFICA SE UM NÚMERO É PAR USANDO FILTER
+# def numeros_pares(n):
+#     return n % 2 == 0
 
-num_par = list(filter(numeros_pares, numeros))
-print(num_par)
+# numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 
+# # Filtra os números pares da lista 'numeros'
+# num_par = list(filter(numeros_pares, numeros))
+# print(num_par)
+#_____________________________________________________________________________________________________________________
+
+# FILTRA E RETORNA OS NÚMEROS ÍMPARES DE UMA LISTA USANDO FILTER E LAMBDA
+# numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+# num_impar = list(filter(lambda x: x % 2 != 0, numeros))
+# print(num_impar)
+#_____________________________________________________________________________________________________________________
+
+# FUNÇÃO REDUCE(): Realiza operações cumulativas em sequências de elementos, também age numa sequências de elementos. Entretanto, seu funcionamento não é muito similar ao filter ou map. Ela acumula valores na sequencia e retorna um único valor no final.
+# OBS: Filter e Map retornam conjuntos de valores, e  a reduce retorna 1 valor só, que é o valor total da aplicação de uma função em cima de cada item de uma lista ou de uma sequência qualquer.
+
+# SINTAXE: reduce(função, sequencia, valor_inicial)
+
+
+# FUNÇÃO QUE MULTIPLICA UM NÚMERO PELO OUTRO ATÉ O FINAL RETORNA O RESULTADO DA MULTIPLICAÇÃO DE TODOS OS NÚMEROS
+# Importa a função 'reduce' do módulo 'functools'.
+from functools import reduce
+
+def mult(x,y):
+    return x * y
+
+numeros = [1,2,3,4,8,6]
+
+total = reduce(mult, numeros)
+print(total)
 
 
 
