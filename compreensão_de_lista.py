f55de6724e2bc9c5563fd62fb6740ce40b7fffc2
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-08-04 00:48:27 -0300

    criei o compreensão de lista.py

---
+++ compreensão_de_lista.py
@@ -0,0 +1,2 @@
+# SINTAXE GERAL: [expressão for item in lista]
+# onde "expressão" é a expressão que será avaliada para cada elemento em "lista", e "item" é a variável que representacada elemento na lista.
