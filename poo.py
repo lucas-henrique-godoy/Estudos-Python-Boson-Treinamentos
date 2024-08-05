d8ae08c30d2d01f11b6057a15154580439becfda
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-08-05 12:22:27 -0300

    atualizei poo.py

---
+++ poo.py
@@ -0,0 +1,10 @@
+# Orientação a Objetos: Paradigma de Programação(estilo/forma de programar).
+# Classes e Objetos
+# Classes: São modelos abstratos que vão representar itens do mundo real dentro do software.
+# Obejetos: Sao as ocorrências dessas classes, são essas classes carregadas na memória efetivamente existindo pra valer.
+
+# SINTAXE CLASSE:
+    # Atributos: São
+    # Métodos:
+     
+
